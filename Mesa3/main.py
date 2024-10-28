e20558b6392992a368ff6b3d0883b752ac420ad8
Angel Moya <141160618+Angelnglz@users.noreply.github.com>
2024-10-28 14:10:46 +0100

    Update main.py

--- Mesa3/main.py
+++ Mesa3/main.py
@@ -15,30 +15,8 @@ counter = 0
 # Create PWM object for the servo
 servo = PWM(Pin(0), freq=50)
 
-myLED = Pin(0, Pin.OUT)
-myLED_rojo = Pin(48, Pin.OUT)
-
-# Function to map an angle (0 to 180) to PWM duty cycle
-
-from machine import Pin, PWM
-import time
-from time import sleep_ms
-from bleradio import BLERadio
-
-# A board can broadcast small amounts of data on one channel. Here we broadcast
-# on channel 5. This board will listen for other boards on channels 4 and 18.
-radio = BLERadio(broadcast_channel=5, observe_channels=[4]) # Aqui podes sumar cuantos canales necesites en la lista!!
-
-# You can run a variant of this script on another board, and have it broadcast
-# on channel 4 or 18, for example. This board will then receive it.
-
-counter = 0
-
-# Create PWM object for the servo
-servo = PWM(Pin(38), freq=50)
-
-myLED = Pin(0, Pin.OUT)
-myLED_rojo = Pin(48, Pin.OUT)
+#myLED = Pin(0, Pin.OUT)
+#myLED_rojo = Pin(48, Pin.OUT)
 
 # Function to map an angle (0 to 180) to PWM duty cycle
 def set_angle(angle):
@@ -57,12 +35,12 @@ while True:
       # It gives None if no data is detected.
       observed = radio.observe(4)
       
-      if observed != None and observed[0] =="B": # Observa el valor del sensor de Distancia
-        myLED.value(0)
+      if observed != None and observed[0] =="B": # 
+        #myLED.value(0)
         set_angle(0)
 
-      if observed != None and observed[0] =="S": # Observa el valor del sensor de Distancia
-        myLED.value(1)
+      if observed != None and observed[0] =="S": #
+        #myLED.value(1)
         set_angle(90)
         
       print(f"Llego --> {observed}")
